4e532be5e70dc67ef3fd4b0e2f2b08e9e9443a91
Neerjhar Goswami <neerjhargoswami@gmail.com>
2025-07-06 12:37:59 +0530

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //including the library
+using namespace std;
+int main(){ //int main is a function for printing the cout value
+  cout<<"hello world"; //c out stands for console out 
+  return 0;
+}
